51ca3f04efe3967369048e98c6f2376d6d469926
Emjacks <emjackson107@gmail.com>
2023-04-17 11:14:03 -0400

    update app 4

--- client/src/App.js
+++ client/src/App.js
@@ -6,33 +6,22 @@ import PostDetails from './components/PostDetails/PostDetails';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
-import Profile from './components/Profile/Profile';
 
 const App = () => {
-  const user = JSON.parse(localStorage.getItem('profile'));
-
-  const authRedirect = () => {
-    return !user ? <Auth /> : <Navigate to="/posts" />;
-  };
-  
-  const authRedirectHome = () => {
-    return <Navigate to="/posts" />;
-  };
   
   return (
     <BrowserRouter>
-    <Container maxWidth="xl">
-      <Navbar />
-      <Routes>
-        <Route path="/" exact element={authRedirectHome()} />
-        <Route path="/posts" exact element={<Home />} />
-        <Route path="/posts/search" exact element={<Home />} />
-        <Route path="/posts/:id" exact element={<PostDetails />} />
-        <Route path="/auth" exact element={authRedirect()} />
-        <Route path="/profile/:id" exact element={<Profile />}/>
-      </Routes>
-    </Container>
-  </BrowserRouter>
+      <Container maxWidth="xl">
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Navigate to="/posts" />} />
+          <Route path="/posts" exact element={<Home />} />
+          <Route path="/posts/search" exact element={<Home />} />
+          <Route path="/posts/:id" exact element={<PostDetails />} />
+          <Route path="/auth" exact element={<Auth />} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
   );
 };
 
--- client/src/components/Profile/Profile.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Profile = () => {
-  return (
-    <div>Profilefffffff</div>
-  )
-}
-
-export default Profile
