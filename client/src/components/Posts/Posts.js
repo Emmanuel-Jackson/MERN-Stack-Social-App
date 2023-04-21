41d62ee65d395d71daa8aefb56de822ebce70119
Emjacks <emjackson107@gmail.com>
2023-04-20 21:25:57 -0400

    updated app 5

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -9,29 +9,41 @@ const Posts = ({ setCurrentId }) => {
   const { posts, isLoading } = useSelector((state) => state.posts);
   const classes = useStyles();
 
-  if (!posts.length && !isLoading) return (
-    <div style={{ position: "absolute", top: "50%", right: "50%", color: "gray" }}>
-    <CloudOffIcon style={{ fontSize: "90px", position: "absolute", bottom: "110%", right: "45%"}}/>
-    <Typography align="center" variant="h4">No results found for your search...</Typography>
-    <Typography align="left" variant="subtitle1">Suggestions:</Typography>
-      <ul align="left">
-        <li>Make sure your words are spelled correctly.</li>
-        <li>Make sure their were no commas or spaces between your hastags</li>
-        <li>Try using different keywords that match the posts.</li>
-      </ul>
-    </div>
-  )
+  if (!posts.length && !isLoading) {
+    return (
+      <div style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>
+        <CloudOffIcon style={{ fontSize: "90px", marginBottom: "20px" }} />
+        <Typography align="center" variant="h4">No results found for your search...</Typography>
+        <Typography align="left" variant="subtitle1">Suggestions:</Typography>
+        <ul align="left">
+          <li>Make sure your words are spelled correctly.</li>
+          <li>Make sure there were no commas or spaces between your hashtags</li>
+          <li>Try using different keywords that match the posts.</li>
+        </ul>
+      </div>
+    );
+  }
 
   return (
-    isLoading ? <Button><CircularProgress />&nbsp; &nbsp; May take a while. Loading posts please wait...</Button> : (
-      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-        {posts?.map((post) => (
-          <Grid key={post._id} item xs={12} sm={12} md={6} lg={4}>
-            <Post post={post} setCurrentId={setCurrentId} />
-          </Grid>
-        ))}
-      </Grid>
-    )
+    <div style={{ position: "relative" }}>
+      {isLoading && (
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+          <CircularProgress color="primary" />
+        </div>
+      )}
+      {isLoading && (
+        <Typography align="center" variant="body1" color="textSecondary" style={{ color: "gray", marginTop: "10px" }}>Loading posts, please wait...</Typography>
+      )}
+      {!isLoading && (
+        <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+          {posts.map((post) => (
+            <Grid key={post._id} item xs={12} sm={12} md={6} lg={4}>
+              <Post post={post} setCurrentId={setCurrentId} />
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </div>
   );
 };
 
